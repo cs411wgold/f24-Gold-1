600eb36fdd65e59ea6de7404b8be4b9d287d1a6b
Angel Mercado <amerc006@odu.edu>
2024-10-04 21:28:46 -0400

    Created forums page.

---
+++ frontend/forums/forums.js
@@ -0,0 +1,12 @@
+
+//Comment
+function showComment() {
+    var commentArea = document.getElementById("comment-area");
+    commentArea.classList.remove("hide");
+}
+
+//Reply
+function showReply() {
+    var replyArea = document.getElementById("reply-area");
+    replyArea.classList.remove("hide");
+}
