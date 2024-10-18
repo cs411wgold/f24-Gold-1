4d77760c9fca3e8c4a745f8bdbc1c6412b17203e
Reggie <rpind002@odu.edu>
2024-10-17 22:27:58 -0400

    Backend with  errors to frontend Dockerfile

--- backend/djangoConfig/settings.py
+++ backend/djangoConfig/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'djangoConfig.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'my-postgres-container'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
