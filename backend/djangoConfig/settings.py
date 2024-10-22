cb1e0c6b6d3861ab59ccc08f6fb15b458a927cf1
gtcatlett <gcatl002@odu.edu>
2024-10-21 21:12:13 -0400

    update login form, minor change to signup form

--- backend/djangoConfig/settings.py
+++ backend/djangoConfig/settings.py
@@ -44,9 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'login',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,8 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-
 ]
 
 # CORS_ALLOWED_ORIGINS = [
--- backend/login/apps.py
+++ backend/login/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'service'
+    name = 'login'
