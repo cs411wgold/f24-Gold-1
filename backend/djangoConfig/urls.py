c93379416f1e41b83a715b44a70763bb85582485
gtcatlett <gcatl002@odu.edu>
2024-10-21 20:17:33 -0400

    add persistent user login record

--- backend/djangoConfig/urls.py
+++ backend/djangoConfig/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import signup_view
+from login.views import signup_view
 
 
 urlpatterns = [
