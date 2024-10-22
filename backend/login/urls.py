35eecd801ebb961749dfbc486103e8873feb96f7
gtcatlett <gcatl002@odu.edu>
2024-10-21 20:36:26 -0400

    update sign up

--- backend/login/urls.py
+++ backend/login/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import hello_world, homepage, login_view
+from .views import hello_world, homepage, login_view, signup_view
 
 urlpatterns = [
     path('hello/', hello_world, name='hello page'),
     path('', homepage, name='landing page yuhhh'),
     path('login/', login_view, name='login'),
+    path('signup/', signup_view, name='signup'),
 ]
