0d5ec4be867277c15ccbb3571bdc4df9230ea4b8
Erin Shanks <eshan003@odu.edu>
2024-10-30 14:02:53 -0400

    Added popups to settings.html and added settings.js

---
+++ frontend/profile/settings.js
@@ -0,0 +1,4 @@
+function openPopup(popupId) { 
+    document.getElementById(popupId).style.display = 'block'; } 
+function closePopup(popupId) { 
+    document.getElementById(popupId).style.display = 'none'; }
