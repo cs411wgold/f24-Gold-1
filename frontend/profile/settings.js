b3022120fc3eb649fd44d3800e535cc2bccc0551
Erin Shanks <eshan003@odu.edu>
2024-11-07 13:11:21 -0500

    Added View/Manage Users

--- frontend/profile/settings.js
+++ frontend/profile/settings.js
@@ -1,17 +1,16 @@
-var modal = document.getElementById("PublicProfile-modal");
-var btn = document.getElementById("PublicProfile");
-var span = document.getElementsByClassName("close")[0];
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
 function dropMenu() {
     document.getElementById("TimeZone").classList.toggle("show");
