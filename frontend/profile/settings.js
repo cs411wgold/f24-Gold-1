b785d58e9166c974ffe1aa803bc87fb472822475
Erin Shanks <eshan003@odu.edu>
2024-11-10 22:35:12 -0500

    Added popup forms to Account

--- frontend/profile/settings.js
+++ frontend/profile/settings.js
@@ -12,5 +12,11 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
-
-
+function openPopup(popupId) { 
+    document.getElementById(popupId).style.display = 'block'; } 
+function closePopup(popupId) { 
+    document.getElementById(popupId).style.display = 'none'; } 
+window.onclick = function(event) { 
+    if (event.target.classList.contains('popup')) 
+        { event.target.style.display = 'none'; }
+}
