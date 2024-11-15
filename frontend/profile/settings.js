11a71c55bbd19a7701dc56eb188b905c777f5fd5
Erin Shanks <eshan003@odu.edu>
2024-11-15 10:41:45 -0500

    Added jsdoc comments to settings.js

--- frontend/profile/settings.js
+++ frontend/profile/settings.js
@@ -1,14 +1,35 @@
+/**
+ * Function to open the popup, after clicking the 
+ * popup button, changeEmail or changePassword.
+ *  
+ * @param {string} popupId - The ID of the popup element to be opened.
+ */
 function openPopup(popupId) { 
     document.getElementById(popupId).style.display = 'block'; } 
-
+/**
+ * Function to close the popup, after clicking the 
+ * close button.
+ * 
+ * @param {string} popupId - The ID of the popup element to be opened.
+ */
 function closePopup(popupId) { 
     document.getElementById(popupId).style.display = 'none'; } 
-    
+/**
+ * Function to close the popup when the user clicks outside of it.
+ * 
+ * @param {MouseEvent} event - The click event. 
+ */    
 window.onclick = function(event) { 
     if (event.target.classList.contains('popup')) 
         { event.target.style.display = 'none'; }
 }
-
+/**
+ * Function to handle change password form submission, 
+ * validation of the inputs on the form, and sends the 
+ * new password to the database. 
+ * 
+ * @param {Event} e - The form submission event.
+ */
 document.getElementById('passwordForm').addEventListener('submit', async function(e) {
     e.preventDefault();
     
